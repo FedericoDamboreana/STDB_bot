2e5ff29cb9eff9a04818c7175460655edc2e93a6
FedericoDamboreana <suarezfede74@gmail.com>
2023-12-13 00:17:30 -0300

    Context summarization fix
    I changed the QA model for a Text2Text model

--- history_manager.py
+++ history_manager.py
@@ -3,26 +3,22 @@ import requests
 class ChatHistoryManager:
     def __init__(self):
         self.chat_history = []
+        self.last_message = ""
         self.headers = {
             "Authorization": "Bearer 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"
         }
     
     def add_ai_message(self, message):
+        self.chat_history.append({"sender": "User", "message": self.last_message})
+        self.last_message = ""
         self.chat_history.append({"sender": "AI", "message": message})
+        
     
     def add_user_message(self, message):
-        self.chat_history.append({"sender": "User", "message": message})
+        self.last_message = message
     
     def get_history(self):
-        history_str = ""
-        for i, m in enumerate(self.chat_history):
-            if i == len(self.chat_history) - 1 and m['sender'] == 'User':
-                history_str += "\nUser's last question: " + m['message']
-            else:
-                history_str += f"{m['sender']}: {m['message']}"
-            if i < len(self.chat_history) - 1:
-                history_str += "\n"
-
+        history_str = "\n".join([f"{m['sender']}: {m['message']}" for m in self.chat_history])
         return history_str
     
     def save_history_to_disk(self, filename):
@@ -38,9 +34,9 @@ class ChatHistoryManager:
     
     def get_optimized_history(self):
         payload = {
-            "inputs": "Given this conversation: \n" + self.get_history() + "\n\nTell me what is the user's last question.\n\nSummary: " ,
+            "inputs": "Given this conversation: \n" + self.get_history() + "\n\nSummary: " ,
         }
-        print(">>> input: ", payload["inputs"])
+
         response = requests.post(
             "https://api-inference.huggingface.co/models/tiiuae/falcon-7b-instruct",
             headers=self.headers,
@@ -52,36 +48,71 @@ class ChatHistoryManager:
         else:
             return f"An error occurred: {response.text}"
         
-    def generate_query(self, history):
-        API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-base"
-        headers = {"Authorization": "Bearer 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"}
-
-        question = "What question do i need to research to answer the last message? \n"
-        context = "Conversation summary:\n" + history
-        
-        input = question + context
+    def generate_query(self, summary):
+        prompt = "\n\nWhat does the user wants to know?\nWhat the user wants to know is:"
         payload = {
-            "inputs": input,
+            "inputs": "This is the summary of a conversation of a user and a AI: " + summary + "\n\nThis is the user's last question: " + self.last_message + prompt,
         }
+        print("=====================================================================")
+        print("\nINPUT\n")
+        print(payload["inputs"])
 
-        response = requests.post(API_URL, headers=headers, json=payload)
+        response = requests.post(
+            "https://api-inference.huggingface.co/models/tiiuae/falcon-7b-instruct",
+            headers=self.headers,
+            json=payload
+        )
         if response.status_code == 200:
-            answer = response.json()[0]["generated_text"]  # Changed 'generated_text' to 'answer'
-            return answer
+            summary = response.json()
+            return summary[0]["generated_text"].split("What the user wants to know is:")[-1].strip()
         else:
             return "Error: " + response.text
 
 
-chat_manager = ChatHistoryManager()
-chat_manager.add_user_message("I'm planning a vacation. Any suggestions?")
-chat_manager.add_ai_message("That sounds exciting! What kind of places do you like?")
-chat_manager.add_user_message("I love beaches and sunny weather.")
-chat_manager.add_ai_message("You might enjoy visiting the Caribbean islands then. They have beautiful beaches.")
-chat_manager.add_user_message("That's a great idea! How's the weather there around this time?")
 
-optimized_history = chat_manager.get_optimized_history()
-print("\n\n>>> optimized history: " + optimized_history)
+chat1 = ChatHistoryManager()
+chat1.add_user_message("I'm planning a vacation. Any suggestions?")
+chat1.add_ai_message("That sounds exciting! What kind of places do you like?")
+chat1.add_user_message("I love beaches and sunny weather.")
+chat1.add_ai_message("You might enjoy visiting the Caribbean islands then. They have beautiful beaches.")
+chat1.add_user_message("That's a great idea! How's the weather there around this time?")
+
+chat2 = ChatHistoryManager()
+chat2.add_user_message("I'm trying to decide what to cook for dinner.")
+chat2.add_ai_message("What kind of cuisine do you enjoy?")
+chat2.add_user_message("I really like Italian food.")
+chat2.add_ai_message("How about making spaghetti carbonara? It's a classic Italian dish.")
+chat2.add_user_message("Good suggestion! Do you have a recipe?")
+
+chat3 = ChatHistoryManager()
+chat3.add_user_message("I'm looking to buy a new laptop. Any advice?")
+chat3.add_ai_message("Sure, what will you be using it for mostly?")
+chat3.add_user_message("Mostly for software development.")
+chat3.add_ai_message("You might want a laptop with a powerful CPU, plenty of RAM, and a solid-state drive for faster performance.")
+chat3.add_user_message("Do you have any specific models in mind?")
+
+chat4 = ChatHistoryManager()
+chat4.add_user_message("I want to watch a movie tonight. Any recommendations?")
+chat4.add_ai_message("What genre do you prefer?")
+chat4.add_user_message("I love science fiction.")
+chat4.add_ai_message("Have you seen 'Interstellar'? It's a great sci-fi movie.")
+chat4.add_user_message("Yes, I loved it! Anything similar?")
+
+chat5 = ChatHistoryManager()
+chat5.add_user_message("I'm thinking of taking up a new sport. What do you suggest?")
+chat5.add_ai_message("That's a great idea! Are you looking for something indoors or outdoors?")
+chat5.add_user_message("Preferably outdoors.")
+chat5.add_ai_message("How about tennis? It's a fun outdoor sport and a great way to stay fit.")
+chat5.add_user_message("Hmm, I've never played tennis. What do I need to start?")
 
 
-answer = chat_manager.generate_query(optimized_history)
-print("\n\n >>> answer: ", answer)
+chats = [chat1, chat2, chat3, chat4, chat5]
+
+for chat in chats:
+    optimized_history = chat.get_optimized_history()
+
+    answer = chat.generate_query(optimized_history)
+    print("\n\n")
+    print("\nGENERATED QUERY\n")
+    print(answer)
+    print("\n=====================================================================")
