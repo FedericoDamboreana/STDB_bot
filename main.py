88b4bd4d802d0e8a3338a6aceb2b844f30cab4ab
FedericoDamboreana <suarezfede74@gmail.com>
2023-12-15 01:27:19 -0300

    State manager fix and implementation in main controller

--- main.py
+++ main.py
@@ -31,53 +31,63 @@ class MainController:
         print(f"Tiempo para cargar la base de datos: {load_db_time - llm_time:.2f} segundos")
         print(f"Tiempo total para inicializar MainController: {total_time - self.start_time:.2f} segundos")
 
-    def get_prompt(self, history):
-        prompt = """
-You are an assistant having a conversation with a user. Be kind.
-This is a summary of the conversation:
-{history}
+    def get_greeting_prompt(self, user_message):
+        return f"""
+You are an AI assistant. Your only task is to respond briefly to the user's message. Do not initiate or continue a conversation beyond the message.
 
-Answer:"""
-        return prompt.format(history=history)
+User's message: {user_message}
+
+Your response (brief and to the point):"""
 
     def get_prompt(self, history, context, query):
         prompt = """
-                You are an assistant having a conversation with a user.
-                Answer the user question briefly and using context provided to get information.
-                This is a summary of the conversation: {history}
-                This is some information to help you answer the user's question: {context}
-                This is the user's question: {query}
+You are an assistant having a conversation with a user.
+Answer the user question briefly and using context provided to get information.
+This is a summary of the conversation: {history}
+This is some information to help you answer the user's question: {context}
+This is the user's question: {query}
 
-                Answer:"""
+Answer:"""
         
         return prompt.format(history=history, context=context, query=query)
     
     #greeting
     def greeting_answer(self, history):
-        prompt = self.get_prompt(history)
-        response = self.llm(prompt)
+        print("==================================================================")
+        print("GREETING FLOW\n\n")
+        prompt = self.get_greeting_prompt(history)
+        print("PROMPT")
+        print(prompt)
+        print("\n\nAI RESPONSE")
+        response = self.llm(prompt, 
+                            max_tokens=30,
+                            temp=0.1,
+                            top_k=10, 
+                            top_p=0.2,)
 
-        print("\n\nAI:", response)
         self.history_manager.add_ai_message(response)
         pass
 
     #qa
     def qa_answer(self, history):
+        print("==================================================================")
+        print("QA FLOW\n\n")
         query = self.history_manager.generate_query(history)
 
         context = self.context_manager.get_optimized_context(query)
 
         prompt = self.get_prompt(history, context, query)
-
+        print("PROMPT")
+        print(prompt)
+        print("\n\nAI RESPONSE")
         response = self.llm(prompt)
 
-        print("\n\nAI:", response)
         self.history_manager.add_ai_message(response)
         pass
 
     def run(self):
         while True:
-            user_message = input("User: ")
+            user_message = input("\n\nUser: ")
             if user_message.lower() == 'salir':
                 break
             self.start_time = time.time()
@@ -85,17 +95,14 @@ Answer:"""
             self.history_manager.add_user_message(user_message)
 
             history = self.history_manager.get_full_history()
-            history_time = time.time()
 
             state = self.state_manager.get_state(history)
+            
             if "question answering" in state:
                 history = self.history_manager.get_optimized_history()
-                history_time = time.time()
                 self.qa_answer(history)
             else:
                 self.greeting_answer(history)
-
-            end_time = time.time()
 
 
 #"Is there a way to create a custom study area in Business Analyst without using rings or drive times?"
