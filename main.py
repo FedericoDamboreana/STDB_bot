718de4506420054e6d49fafedeeb34d27dd7470c
h92web <h92web@gmail.com>
2023-12-18 15:40:13 -0300

    cambios generales en prompts

--- main.py
+++ main.py
@@ -11,14 +11,18 @@ class MainController:
 
         self.history_manager = HistoryManager()
         history_manager_time = time.time()
+        print(">>> history manager")
 
         self.state_manager = StateManager()
+        print(">>> state manager")
 
         self.context_manager = ContextManager("./store")
         context_manager_time = time.time()
+        print(">>> context manager")
         
-        self.llm = GPT4All(model="./models/gpt4all-falcon-q4_0.gguf", callbacks=[StreamingStdOutCallbackHandler()], verbose=True, n_threads=10)
+        self.llm = GPT4All(model="./models/gpt4all-falcon-q4_0.gguf", callbacks=[StreamingStdOutCallbackHandler()], verbose=True, n_threads=6)
         llm_time = time.time()
+        print(">>> LLM model")
         
         self.context_manager.load_db()
         load_db_time = time.time()
@@ -31,38 +35,39 @@ class MainController:
         print(f"Tiempo para cargar la base de datos: {load_db_time - llm_time:.2f} segundos")
         print(f"Tiempo total para inicializar MainController: {total_time - self.start_time:.2f} segundos")
 
-    def get_prompt(self, history):
-        prompt = """
-You are an assistant having a conversation with a user. Be kind.
-This is a summary of the conversation:
-{history}
-
-Answer:"""
+    def get_prompt_greeting(self, history):
+        prompt = f"""
+You are an AI assistant. Your only task is to respond briefly to the user's message. Do not initiate or continue a conversation beyond the greeting. 
+Conversation history: "{history}"
+Your response (brief and to the point):"""
         return prompt.format(history=history)
 
     def get_prompt(self, history, context, query):
         prompt = """
-                You are an assistant having a conversation with a user.
-                Answer the user question briefly and using context provided to get information.
-                This is a summary of the conversation: {history}
-                This is some information to help you answer the user's question: {context}
-                This is the user's question: {query}
+You are an assistant having a conversation with a user.
+Answer the user question briefly and using context provided to get information.
+This is a summary of the conversation: {history}
+This is some information to help you answer the user's question: {context}
+This is the user's question: {query}
 
-                Answer:"""
+Answer:"""
         
         return prompt.format(history=history, context=context, query=query)
     
     #greeting
     def greeting_answer(self, history):
-        prompt = self.get_prompt(history)
+        print("=======================================================")
+        print("GREETING FLOW")
+        prompt = self.get_prompt_greeting(history)
         response = self.llm(prompt)
-
         print("\n\nAI:", response)
         self.history_manager.add_ai_message(response)
         pass
 
     #qa
     def qa_answer(self, history):
+        print("=======================================================")
+        print("QA FLOW")
         query = self.history_manager.generate_query(history)
 
         context = self.context_manager.get_optimized_context(query)
@@ -98,7 +103,7 @@ Answer:"""
             end_time = time.time()
 
 
-#"Is there a way to create a custom study area in Business Analyst without using rings or drive times?"
+#"hello"
 
 
 controller = MainController()
